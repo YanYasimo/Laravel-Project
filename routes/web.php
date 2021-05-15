598ca0e69e86f1a783721af62c90ffbc29fcf8ad
YanYasimo <yanmoura142529@gmail.com>
2021-05-15 19:15:07 -0300

    feature: create static routes

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,23 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Site\HomeController; 
+use App\Http\Controllers\Site\CategoryController; 
+use App\Http\Controllers\Site\BlogController; 
+use App\Http\Controllers\Site\ContactController; 
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::namespace('Site')->group(function () {
 
-Route::get('/', function () {
-    return view('welcome');
+    Route::get('/', [HomeController::class, 'index']);
+
+    Route::get('/products', [CategoryController::class, 'index']);
+    Route::get('/products/{slug}', [CategoryController::class, 'show']);
+
+
+    Route::get('/blog', [BlogController::class, 'index']);
+
+    Route::view('/about', 'site.about.index');
+    
+    Route::get('/contact', [ContactController::class, 'index']);
+    Route::post('/contact', [ContactController::class, 'contact']);
 });
